ed0637d00d20fbd917f28f22b6b76815258989c9
CJosephW <wisecameron1@gmail.com>
2020-04-21 23:45:39 -0400

    added some formatting for additional weather information and an optional unit argument

--- pyweatherv2.py
+++ pyweatherv2.py
@@ -14,16 +14,41 @@ parser = argparse.ArgumentParser(description="Return the current weather and con
 parser.add_argument('Location', metavar='location',
 type=str, help = 'add a location(ex: Grand Rapids)')
 
+parser.add_argument('--unit', metavar='unit', #optional unit argument 
+type =str, help = 'choose a unit, f(fahrenheit), s(scientific) or leave the default metric')
+
 args = parser.parse_args()
 
 location = args.Location #setting location to the argument location
 
-r = requests.get('http://api.weatherstack.com/current?access_key='+api_secret+'& query='+location)
+if args.unit != None:
+    unit = args.unit
+    r = requests.get('http://api.weatherstack.com/current?access_key='+api_secret+'& query='+location+ '& units=' +unit)
+
+else:
+    r = requests.get('http://api.weatherstack.com/current?access_key='+api_secret+'& query='+location)
+
 data = r.content
 weather_json = json.loads(data)
 current = weather_json['current']
 
+
+
 print("In "+location+" it is: " + str(current['temperature'])+" degrees Celsius and")
+
+def displayWeatherTable(parsedJson): # function to print weather table to prevent clutter
+    print("""
+=========================
+| Feels Like: {} |   
+| Wind Speed: {} |
+| Wind Direction: {} | 
+| Humidity:  {} |
+| Percipitation: {} |
+| Cloud Cover: {} |
+| Visibility: {} | 
+=========================
+    """.format(parsedJson['feelslike'], parsedJson['wind_speed'], parsedJson['wind_dir'],
+    parsedJson['humidity'], parsedJson['precip'], parsedJson['cloudcover'], parsedJson['visibility']))       
 
 if str(current['weather_descriptions'][0]) == "Partly cloudy":#checking weather description(Ex: clear, sunny, cloudy)
 
@@ -35,6 +60,7 @@ if str(current['weather_descriptions'][0]) == "Partly cloudy":#checking weather 
                  \                          .'
                    \ ~- . _____________ . -~ 
                     """)
+    displayWeatherTable(current)
 elif str(current['weather_descriptions'][0]) == "Sunny":
 
     print("""033[1;33m SUNNY
@@ -48,9 +74,11 @@ elif str(current['weather_descriptions'][0]) == "Sunny":
    '   / `!` \   `
       ;   :   ; 
                     """)
+    displayWeatherTable(current)
 elif str(current['weather_descriptions'][0]) == "Clear":
+    
  #in the next line I am changing the output color with the escape code "\033" setting the style to 1(normal) and setting the color to 34 which is blue
-    print("""\033[1;34m CLEAR
+    print("""\033[1;34m 
       _                 
      | |                
   ___| | ___  __ _ _ __ 
@@ -58,3 +86,6 @@ elif str(current['weather_descriptions'][0]) == "Clear":
 | (__| |  __/ (_| | |   
  \___|_|\___|\__,_|_|   
  """)
+    displayWeatherTable(current)
+
+
